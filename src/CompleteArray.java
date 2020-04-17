9df04b366104ec2215f5bd25f42c248451f67db7
eyasuh <eyha12@gmail.com>
2020-04-17 15:23:36 -0400

    first commit

---
+++ src/CompleteArray.java
@@ -0,0 +1,12 @@
+public class CompleteArray {
+    public static void main(String[] args) {
+        int[] numbers = {2, 29, 33, 4, 15, 62, 7, 81, 9, 10};
+
+        // Declare sum variable, assign it initial value of zero
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
+    }
+}
